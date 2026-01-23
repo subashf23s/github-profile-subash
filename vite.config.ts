c714d19ab6a6e28e0b35fd42b68aade00b669927
Subash <subashsuni55@gmail.com>
2026-01-23 11:12:55 +0530

    added github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/github-profile-subash",
   plugins: [tailwindcss(), react()],
 });
