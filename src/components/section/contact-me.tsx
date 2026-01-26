09f0527d8cb960c071e456862939e2c7c778eb26
Subash <subashsuni55@gmail.com>
2026-01-26 09:36:02 +0530

    initialized contact me

--- src/components/section/contact-me.tsx
+++ src/components/section/contact-me.tsx
@@ -1,7 +1,14 @@
 const ContactMe = () => {
   return (
-    <section className="container min-h-dvh" id="contact-me">
-      <h1>Contact me</h1>
+    <section className="container min-h-dvh py-8" id="contact-me">
+      <div className="grid place-items-center text-center">
+        <p>Connect with me</p>
+        <h2 className="text-5xl py-4">Get in touch</h2>
+        <p className="w-3/4 leading-10 tracking-wider">
+          I'd love to hear from you! If you have any questions, comments or
+          feedback, please use the form below.
+        </p>
+      </div>
     </section>
   );
 };
