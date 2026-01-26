5572c242e8c59dd3d2a30232da61062bb030b777
Subash <subashsuni55@gmail.com>
2026-01-26 09:35:46 +0530

    initialized my work

--- src/components/section/my-work.tsx
+++ src/components/section/my-work.tsx
@@ -1,7 +1,14 @@
 const MyWork = () => {
   return (
-    <section id="my-work" className="container min-h-dvh">
-      <h1>My work</h1>
+    <section id="my-work" className="container min-h-dvh py-8">
+      <div className="grid place-items-center text-center">
+        <p>My Portfolio</p>
+        <h2 className="text-5xl py-4">My latest work</h2>
+        <p className="w-3/4 leading-10 tracking-wider">
+          Welcome to my web development portfolio! Explore a collection of
+          projects showcasing my expertise in fullstack development.
+        </p>
+      </div>
     </section>
   );
 };
