61503224f6df6849ab05a88579cc7d671758b724
Subash <subashsuni55@gmail.com>
2026-01-28 09:50:20 +0530

    initialized footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ContactMe from "./components/section/contact-me";
 import Home from "./components/section/home";
 import MyWork from "./components/section/my-work";
 import Services from "./components/section/services";
+import Footer from "./components/ui/footer";
 import Header from "./components/ui/header";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Services />
       <MyWork />
       <ContactMe />
+      <Footer />
     </main>
   );
 }
